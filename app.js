b875b5e5934d8c6e92aae7618d172235c8247d3d
jesus omana <jesus.omanamarquez@gmail.com>
2023-11-06 22:32:51 -0300

    se arreglo al app.js la formula no correspondia al cometido del ejercicio y se verifico que la pagina fuera responsive y se corrigio lo que no respondia al responsive (footer)

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const form = document.getElementById("form");
+const element = document.querySelector("form");
 
-form.addEventListener("submit", (event) => {
-  alert("se envio");
+element.addEventListener("submit", (event) => {
+  alert(
+    "Seguro No Pagamos Cooperativa recibio tu formulario, en los proximos minutos uno de nuestros asesores se contactara con usted, gracias por preferirnos"
+  );
 });
+
+console.log("hola");
